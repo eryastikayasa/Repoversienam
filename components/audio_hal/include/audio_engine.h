b67df2d0eaed3d68e720ed3fd02b057b3e76e8cb
eryastikayasa <eryastikayasa@gmail.com>
2026-09-12 21:26:17 +0800

    audio: expose runtime RAM diagnostics

--- components/audio_hal/include/audio_engine.h
+++ components/audio_hal/include/audio_engine.h
@@ -13,6 +13,7 @@ typedef void (*audio_engine_mic_sink_cb_t)(const uint8_t *pcm, size_t len, void 
 bool audio_engine_init(void); audio_engine_state_t audio_engine_get_state(void); const char *audio_engine_state_name(audio_engine_state_t state); bool audio_engine_turn_active(void); const audio_engine_turn_t *audio_engine_get_turn(void); void audio_engine_notify(audio_engine_event_type_t event, uint32_t generation);
 bool audio_engine_push_model_audio(const uint8_t *pcm, size_t len, uint32_t generation); bool audio_engine_push_model_audio_base64(const char *b64, size_t len, uint32_t generation);
 bool audio_engine_set_mic_listener(audio_engine_mic_frame_cb_t cb, void *ctx); bool audio_engine_set_mic_sink(audio_engine_mic_sink_cb_t cb, void *ctx); bool audio_engine_start_capture(void); void audio_engine_start_input_session(void); void audio_engine_stop_input_session(void); bool audio_engine_input_session_active(void);
+void audio_engine_log_diagnostics(const char *stage);
 #ifdef __cplusplus
 }
 #endif
