2097c7580e58107350896626223159ab92815d28
eryastikayasa <eryastikayasa@gmail.com>
2026-09-12 19:48:26 +0800

    fix: make wake word lifecycle re-armable

--- components/wakeword/include/wakeword.h
+++ components/wakeword/include/wakeword.h
@@ -10,7 +10,9 @@ typedef void (*wakeword_detected_cb_t)(void *ctx);
 
 bool wakeword_init(void);
 bool wakeword_start(wakeword_detected_cb_t cb, void *ctx);
+bool wakeword_rearm(void);
 bool wakeword_is_ready(void);
+bool wakeword_is_armed(void);
 
 #ifdef __cplusplus
 }
