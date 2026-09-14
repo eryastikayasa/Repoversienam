4ab8e9941db8f395c8bec9c42296c467a036ae93
eryastikayasa <eryastikayasa@gmail.com>
2026-09-14 08:43:19 +0800

    perf: parse Gemini JSON once

--- components/websocket/include/gemini_message.h
+++ components/websocket/include/gemini_message.h
@@ -3,6 +3,12 @@
 #include <stddef.h>
 #include <stdint.h>
 
+typedef struct cJSON cJSON;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef enum {
     GEMINI_MESSAGE_UNKNOWN = 0,
     GEMINI_MESSAGE_SETUP,
@@ -12,11 +18,8 @@ typedef enum {
     GEMINI_MESSAGE_ERROR
 } gemini_message_type_t;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 gemini_message_type_t gemini_message_classify(const char *json, size_t len);
+gemini_message_type_t gemini_message_classify_root(const cJSON *root);
 
 #ifdef __cplusplus
 }
