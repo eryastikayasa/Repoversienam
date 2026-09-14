98e5bddc2a19630dac1cc8a6aeb7641d1f1f0394
eryastikayasa <eryastikayasa@gmail.com>
2026-09-14 09:53:52 +0800

    OLED: add centralized system state helper

--- components/display/display_engine/display_engine.h
+++ components/display/display_engine/display_engine.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "display_face.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -7,6 +9,9 @@ extern "C" {
 void display_engine_init(void);
 void display_engine_start(void);
 void display_engine_stop(void);
+
+/* Single lightweight presentation entry point for system state. */
+void display_set_system_state(face_state_t face, const char *status);
 
 #ifdef __cplusplus
 }
