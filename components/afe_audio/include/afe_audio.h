f67b9e8e70b851b2ae4aef87dfbaf1fc122c0011
eryastikayasa <eryastikayasa@gmail.com>
2026-09-18 22:55:34 +0800

    separate AFE output drain API

--- components/afe_audio/include/afe_audio.h
+++ components/afe_audio/include/afe_audio.h
@@ -19,6 +19,9 @@ bool afe_audio_is_ready(void);
 bool afe_audio_process(const int16_t *input, size_t input_samples,
                        int16_t *output, size_t output_capacity_samples,
                        size_t *output_samples);
+/* Drain one already-processed AFE output frame without feeding new input. */
+bool afe_audio_fetch_output(int16_t *output, size_t output_capacity_samples,
+                            size_t *output_samples);
 
 int afe_audio_get_feed_samples(void);
 int afe_audio_get_fetch_samples(void);
