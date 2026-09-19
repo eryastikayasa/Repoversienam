d1a14c0b73a1d0696d913ed305d9aca9c31649a2
eryastikayasa <eryastikayasa@gmail.com>
2026-09-19 10:12:19 +0800

    add standby response state API

--- components/websocket/include/websocket_internal.h
+++ components/websocket/include/websocket_internal.h
@@ -43,6 +43,8 @@ void websocket_disconnect(void);
 void websocket_reset_started(void);
 void websocket_request_standby(void);
 bool websocket_standby_requested(void);
+bool websocket_standby_response_started(void);
+bool websocket_standby_timeout_expired(void);
 void websocket_clear_standby_request(void);
 bool websocket_cleanup_is_pending(void);
 void websocket_cleanup_complete(void);
